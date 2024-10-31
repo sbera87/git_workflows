961f6ea5e391c2dd04ba0d58f3b372efb23343a7
Soumava Bera <sberas@amazon.com>
2024-10-31 18:36:06 -0400

    fixes

---
+++ src/hello_world.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+namespace {
+
+int factorials(int n) {
+  if (n <= 1)
+
+    return 1;
+
+  return n * factorials(n - 1);
+}
+
+int factorials2(int n) {
+  if (n <= 1)
+
+                return 1;
+
+  return n * factorials2(n - 1);
+}
+
+
+} // namespace
+
+int main() {
+  int number = 5;
+  std::cout << "Factorial of " << number << " is " << factorials(number) << "\n";
+  return 0;
+}
+
+//dummy
