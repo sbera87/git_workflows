6f0bb3bdaa679ac9d88bfdaf6ba9deef8a70eaf4
Soumava Bera <sberas@amazon.com>
2024-11-05 11:34:29 -0500

    formatted

--- src/hello_world.cpp
+++ src/hello_world.cpp
@@ -3,23 +3,20 @@
 namespace {
 
 int factorials(int n) {
-  if (n <= 1)
-    return 1;
+  if (n <= 1) return 1;
   return n * factorials(n - 1);
 }
 
 int factorials2(int n) {
-  if (n <= 1)
-    return 1;
+  if (n <= 1) return 1;
   return n * factorials2(n - 1);
 }
 
-} // namespace
+}  // namespace
 
 int main() {
   int number = 5;
-  std::cout << "Factorial of " << number << " is " << factorials(number)
-            << "\n";
+  std::cout << "Factorial of " << number << " is " << factorials(number) << "\n";
   return 0;
 }
 
